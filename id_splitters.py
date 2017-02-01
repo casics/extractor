ab95f5cd9c13ae08a7f2674798d258c942437cdd
Michael Hucka <mhucka@caltech.edu>
2017-01-31 20:17:31 -0800

    Simplest id splitter implemented.

---
+++ id_splitters.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+# @file    id_splitters.py
+# @brief   ID splitters
+# @author  Michael Hucka
+#
+# <!---------------------------------------------------------------------------
+# Copyright (C) 2015 by the California Institute of Technology.
+# This software is part of CASICS, the Comprehensive and Automated Software
+# Inventory Creation System.  For more information, visit http://casics.org.
+# ------------------------------------------------------------------------- -->
+
+import bs4
+import chardet
+from   datetime import datetime
+from   fnmatch import fnmatch
+import html2text
+import locale
+import io
+import keyword
+import magic
+import markdown
+import math
+import nltk
+import operator
+import os
+import pickle
+import plac
+import pprint
+import pypandoc
+import re
+import sys
+import tempfile
+import textile
+from   time import sleep
+from   timeit import default_timer as timer
+from   tokenize import tokenize, COMMENT, STRING, NAME
+from   nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+import unicodedata
+
+sys.path.append('../database')
+sys.path.append('../detector')
+sys.path.append('../cataloguer')
+sys.path.append('../common')
+
+from utils import *
+
+import constants
+from   content_inferencer import *
+from   human_language import *
+from   logger import *
+
+
+# Delimiter-based splitter
+# .............................................................................
+#
+# This does nothing fancy. It splits by explicit delimiter characters like '_'.
+
+_delimiter_chars = '_.:'
+_delimiter_splitter = str.maketrans(_delimiter_chars, ' '*len(_delimiter_chars))
+
+def delimiter_split(identifier):
+    '''Split identifier by explicit delimiters only.'''
+    parts = str.translate(identifier, _delimiter_splitter).split(' ')
+    parts = [p for p in parts if p]
+    return parts
