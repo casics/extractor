9d98ffe204c44f63919943d7afbb4b736ea4fe38
Michael Hucka <mhucka@caltech.edu>
2017-11-28 20:06:28 -0800

    Import constants module

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,3 +1,4 @@
+from .constants import *
 from .file_parser import file_elements
 from .dir_parser import dir_elements
 from .text_extractor import extract_text, clean_plain_text, tokenize_text, all_words, word_frequencies, tabulate_frequencies
