56ba8dfd5ddeb64fbf54ca9b9d13bfae6d1923e8
Michael Hucka <mhucka@caltech.edu>
2017-12-17 13:41:13 -0800

    Rename extractor.py -> extractor_client.py

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -2,4 +2,4 @@ from .constants import *
 from .file_parser import file_elements
 from .dir_parser import dir_elements
 from .text_extractor import extract_text, clean_plain_text, tokenize_text, all_words, word_frequencies, tabulate_frequencies
-from .extractor import Extractor
+from .extractor_client import Extractor
